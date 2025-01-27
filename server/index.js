c50a1edc6859c8fe145b7ba2443d952425d364d1
harikesh <138229790+harik3shhh@users.noreply.github.com>
2025-01-27 08:02:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const PORT = process.env.PORT;
 
 const corsOptions = {
-    origin: "https://blog-dist-file-mksk.vercel.app/",
+    origin: "*",
     methods: "GET, POST, PUT, DELETE, HEAD",
     credentials: true,
 };
